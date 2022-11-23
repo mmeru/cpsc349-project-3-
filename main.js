6d318a40048d75397bc4836344b3df8103fe53fd
Angel Armendariz <95111582+Angel-Armendariz@users.noreply.github.com>
2022-11-22 23:13:01 -0800

    added letter buttons and styling framework

---
+++ main.js
@@ -0,0 +1,3 @@
+//For modules you create another JS file and use export default on the main class and export on the functions as a prefix
+//To import these modules you would for example say import User from '/user.js' if the default class was User
+//Another way shown in MDN web docs is export { name, draw, reportArea, reportPerimeter }; on the module and import { name, draw, reportArea, reportPerimeter } from './modules/square.js'; in the JS file
