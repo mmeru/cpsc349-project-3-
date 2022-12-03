3e9999e6f6815732db42de22a897636663bc072e
Angel Armendariz <angela126509@gmail.com>
2022-12-03 03:09:24 -0800

    made live version show main features but no reset

--- modules/createGuesses.js
+++ modules/createGuesses.js
@@ -5,7 +5,6 @@
   alphabetButton19, alphabetButton20, alphabetButton21, alphabetButton22, alphabetButton23, alphabetButton24,
   alphabetButton25, alphabetButton26
 } from '../../modules/letterButtons.js' */
-import { word } from '../main.js'
 // Make the program choose a random word
 export const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 export const guesses = ['CAT', 'PANDA', 'MONKEY', 'BIRD']
@@ -20,10 +19,14 @@ arrayOfImages.push('/hangman-images/7.jpg')
 arrayOfImages.push('/hangman-images/6.jpg')
 arrayOfImages.push('/hangman-images/5.jpg')
 arrayOfImages.push('/hangman-images/4.jpg')
-
 export const secretWord = []
 export const secretWordDiff = []
-
+export let word = guesses[Math.floor(Math.random() * guesses.length)]
+for (let i = 0; i < word.length; i++) {
+  if (alphabet.includes(word[i])) {
+    secretWordDiff.push('_')
+  }
+}
 /*
 for (let i=0; i<word.length; i++){
     if (alphabet.includes(word[i])){
@@ -44,6 +47,137 @@ export function hiddenWord (word) {
     }
   }
 }
+
+// Make the buttons unclickable after being clicked once
+export const alphabetButton1 = document.getElementById('1')
+alphabetButton1.addEventListener('click', () => {
+  alphabetButton1.className = 'alphabet afterClicked'
+})
+export const alphabetButton2 = document.getElementById('2')
+alphabetButton2.addEventListener('click', () => {
+  alphabetButton2.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton3 = document.getElementById('3')
+alphabetButton3.addEventListener('click', () => {
+  alphabetButton3.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton4 = document.getElementById('4')
+alphabetButton4.addEventListener('click', () => {
+  alphabetButton4.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton5 = document.getElementById('5')
+alphabetButton5.addEventListener('click', () => {
+  alphabetButton5.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton6 = document.getElementById('6')
+alphabetButton6.addEventListener('click', () => {
+  alphabetButton6.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton7 = document.getElementById('7')
+alphabetButton7.addEventListener('click', () => {
+  alphabetButton7.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton8 = document.getElementById('8')
+alphabetButton8.addEventListener('click', () => {
+  alphabetButton8.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton9 = document.getElementById('9')
+alphabetButton9.addEventListener('click', () => {
+  alphabetButton9.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton10 = document.getElementById('10')
+alphabetButton10.addEventListener('click', () => {
+  alphabetButton10.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton11 = document.getElementById('11')
+alphabetButton11.addEventListener('click', () => {
+  alphabetButton11.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton12 = document.getElementById('12')
+alphabetButton12.addEventListener('click', () => {
+  alphabetButton12.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton13 = document.getElementById('13')
+alphabetButton13.addEventListener('click', () => {
+  alphabetButton13.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton14 = document.getElementById('14')
+alphabetButton14.addEventListener('click', () => {
+  alphabetButton14.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton15 = document.getElementById('15')
+alphabetButton15.addEventListener('click', () => {
+  alphabetButton15.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton16 = document.getElementById('16')
+alphabetButton16.addEventListener('click', () => {
+  alphabetButton16.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton17 = document.getElementById('17')
+alphabetButton17.addEventListener('click', () => {
+  alphabetButton17.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton18 = document.getElementById('18')
+alphabetButton18.addEventListener('click', () => {
+  alphabetButton18.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton19 = document.getElementById('19')
+alphabetButton19.addEventListener('click', () => {
+  alphabetButton19.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton20 = document.getElementById('20')
+alphabetButton20.addEventListener('click', () => {
+  alphabetButton20.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton21 = document.getElementById('21')
+alphabetButton21.addEventListener('click', () => {
+  alphabetButton21.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton22 = document.getElementById('22')
+alphabetButton22.addEventListener('click', () => {
+  alphabetButton22.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton23 = document.getElementById('23')
+alphabetButton23.addEventListener('click', () => {
+  alphabetButton23.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton24 = document.getElementById('24')
+alphabetButton24.addEventListener('click', () => {
+  alphabetButton24.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton25 = document.getElementById('25')
+alphabetButton25.addEventListener('click', () => {
+  alphabetButton25.className = 'alphabet afterClicked'
+})
+
+export const alphabetButton26 = document.getElementById('26')
+alphabetButton26.addEventListener('click', () => {
+  alphabetButton26.className = 'alphabet afterClicked'
+})
+
 // Make a function to recognize each letter and set the array to the new letter instead of the _ it has
 export function secretWordChange () {
   for (let i = 0; i < word.length; i++) {
